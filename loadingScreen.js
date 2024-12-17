ce8e82dd548aef8105c5a387dcf7361b1bdeb376
Umar Siddiqui <umarsiddiqui64@gmail.com>
2024-12-17 19:58:27 +0530

    Add loading screen simulation using setInterval and clearInterval

---
+++ loadingScreen.js
@@ -0,0 +1,10 @@
+console.log("Loading..."); 
+
+let loadingInterval = setInterval(() => {
+  console.log("Loading...");
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(loadingInterval); 
+  console.log("Loaded successfully!"); 
+}, 5000);
